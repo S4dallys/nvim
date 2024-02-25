c439e0272b1ce0c49e16c0ee673939f334976c90
S4dallys <geometrygaming8@gmail.com>
2024-02-25 19:47:39 +0800

    <ld>pv and <ld>gs updated to open in new small buffer

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+vim.keymap.set("n", "<leader>gs", ":G<CR>:horizontal resize 8<CR>");
--- lua/sand7/remap.lua
+++ lua/sand7/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", "<Esc>:Lex<CR>:vertical resize 30<CR>")
+vim.keymap.set("i", "<leader>pv", "<Esc>:Lex<CR>:vertical resize 30<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
