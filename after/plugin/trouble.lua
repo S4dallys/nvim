796a9e612b977898a48065b58d3828c3e091c152
S4dallys <geometrygaming8@gmail.com>
2024-03-05 11:18:28 +0800

    trouble configurations

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<leader>tt", function() vim.cmd("TroubleToggle document_diagnostics") end)
-vim.keymap.set("n", "<leader>td", function() vim.cmd("TodoTrouble") end)
+vim.keymap.set("n", "<leader>pt", function() vim.cmd("TodoTelescope") end)
 local signs = { Error = "", Warn = "", Hint = "󰌵", Info = "" }
 
 for type, icon in pairs(signs) do
