874fbfd07bd4d9b63aa889f0d732d66a6c07e734
S4dallys <geometrygaming8@gmail.com>
2024-03-08 00:55:28 +0800

    small changes

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,12 +1,18 @@
 local custom = require'lualine.themes.palenight'
 
 require('lualine').setup {
-    options = { theme  = custom },
+    options = {
+        theme = custom,
+        -- component_separators = { left = '', right = ''},
+        -- section_separators = { left = '', right = ''},
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+    },
     sections = {
         lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff'},
-        lualine_c = {'filename', 'diagnostics'},
-        lualine_x = {'filetype', 'encoding', 'location'},
+        lualine_b = {'branch' },
+        lualine_c = {'filename','diagnostics'},
+        lualine_x = { 'diff', 'filetype', 'encoding', 'location'},
         lualine_y = {},
         lualine_z = {}
     }
