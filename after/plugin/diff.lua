6acd5f723ca0c2c2121b989bd9a03a20bd34cede
S4dallys <geometrygaming8@gmail.com>
2024-03-20 00:58:22 +0800

    rebase

--- after/plugin/diff.lua
+++ after/plugin/diff.lua
@@ -1,3 +1,3 @@
 vim.api.nvim_set_hl(0, "DiffAdd", { bg="#25634f" })
-vim.api.nvim_set_hl(0, "DiffDelete", { bg="#7c3a3a" })
+vim.api.nvim_set_hl(0, "DiffDelete", { bg="#743a3a" })
 vim.api.nvim_set_hl(0, "DiffChanged", { bg="#ffda54" })
