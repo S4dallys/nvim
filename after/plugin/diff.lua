b75ef7525a8a0c13404a440ac245dabc3b337e87
S4dallys <geometrygaming8@gmail.com>
2024-03-20 01:00:23 +0800

    Revert "better fugititve hls"
    This reverts commit 2e0b8258d32237d23f65903cfef83f33a37123e4.

--- after/plugin/diff.lua
+++ after/plugin/diff.lua
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 vim.api.nvim_set_hl(0, "DiffAdd", { bg="#25634f" })
 vim.api.nvim_set_hl(0, "DiffDelete", { bg="#743a3a" })
 vim.api.nvim_set_hl(0, "DiffChanged", { bg="#ffda54" })
+=======
+vim.api.nvim_set_hl(0, "DiffAdd", { fg="#48fa8f" })
+vim.api.nvim_set_hl(0, "DiffDelete", { fg="#fc6a6a" })
+vim.api.nvim_set_hl(0, "DiffChanged", { fg="#ffda54" })
+>>>>>>> parent of 2e0b825 (better fugititve hls)
